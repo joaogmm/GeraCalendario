ffdcbf6c4938e2a1467598768ca869725711eb24
JOÃO-PC\João <jgmmagalhaes@hotmail.com>
2017-12-07 22:12:39 -0200

    Initial commit

---
+++ CalendarioAula-MVC/src/calendarioEstudante/Main.java
@@ -0,0 +1,39 @@
+package calendarioEstudante;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+
+
+public class Main extends Application {
+
+	Text textCounter;
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		
+		textCounter = new Text();
+		Parent root = FXMLLoader.load(getClass().getResource("/view/CalendarFXML.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Gerador de calendário");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+
+	}
+	
+	
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	
+	
+}
